ac1e2d15f331176be779dc13152a0258e9141ce1
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2025-04-24 20:02:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Updating and upgrading packages..."
 sudo apt update && sudo apt upgrade -y
 
 # Move otter.sh to /usr/local/bin and rename it to just "otter"
-echo "Moving and renaming otter.sh to /usr/local/bin/otter..."
+echo "Moving and renaming otter.sh to /usr/local/bin/..."
 sudo mv otter.sh /usr/local/bin/
 cd /usr/local/bin/
 mv otter.sh otter
